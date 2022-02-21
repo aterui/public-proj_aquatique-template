0e347db545fd4775e6ffcb739fbce09c37e2a035
aterui <hanabi0111@gmail.com>
2022-02-21 17:08:37 -0500

    Update figure_script.R

---
+++ code/figure_script.R
@@ -0,0 +1,8 @@
+# This is a sample script for figures
+# As a rule of thumb, follow the following rules:
+#
+# - make a code block
+#   - "Ctr + Shift + R" is a short cut key for code block
+#   - any > 4 leading "#" or "-" after comment text creates a code section
+# - annotate your codes in case you forget what you did
+# 
